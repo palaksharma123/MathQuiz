fcaac0096e08a82fc449ae6c04ca8634f2e05136
palaksharma123 <73351141+palaksharma123@users.noreply.github.com>
2021-06-02 03:37:09 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function log_in() {
+
+  player1_name = document.getElementById("player1_name").value;
+  player2_name = document.getElementById("player2_name").value;
+
+    localStorage.setItem("player1_name", player1_name);
+    localStorage.setItem("player2_name", player2_name);
+
+    window.location.href = "game.html";
+    console.log("userLogedIn");
+
+    }
+  
+  
